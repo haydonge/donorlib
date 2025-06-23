bbe40e0ae2c5d3ba4cf8e06ecd2ab4523fb94dfb
haydonge <haydonge@gmail.com>
2025-06-23 09:33:30 +0800

    add faq

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,20 +9,21 @@ export default defineConfig({
 			title: 'DONOR LIB',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
+				
+				{
+					label: 'Best',
+					autogenerate: {
+						 directory: 'best',
+						 },
+				},
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: '导航', slug: 'guides/example' },
+						{ label: 'FAQ', slug: 'guides/faq'}
 					],
 				},
-				{
-					label: 'Best',
-					autogenerate: {
-						 directory: 'reference',
-						 },
-				},
-
 				{
 					label: 'Smoothbaby',
 					autogenerate: {
